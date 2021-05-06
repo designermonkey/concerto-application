39379b3b46c172301fcbd2f0ec6ba9a6b4123255
John Porter <john@designermonkey.co.uk>
2021-05-06 21:33:33 +0100

    moved to another package

--- src/ResponseBoundary.php
+++ src/ResponseBoundary.php
@@ -2,7 +2,14 @@
 
 namespace Jorpo\Concerto\Application;
 
+/**
+ * @template TResponseModel
+ * @template TResponsePayload of ResponsePayload<TResponseModel>
+ */
 interface ResponseBoundary
 {
+    /**
+     * @param TResponsePayload $payload
+     */
     public function respond(ResponsePayload $payload): void;
 }
