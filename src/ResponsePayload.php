8782438b0f1f3a8e6e9fea9d071a0874be3d7b99
John Porter <john@designermonkey.co.uk>
2021-05-12 21:55:02 +0100

    add request model to payload

--- src/ResponsePayload.php
+++ src/ResponsePayload.php
@@ -7,28 +7,29 @@ use Jorpo\Concerto\Application\Status;
 use Jorpo\ObjectAccess\ImmutableObjectAccess;
 use Traversable;
 
-/**
- * @template TModel of Model
- */
 class ResponsePayload implements IteratorAggregate
 {
     use ImmutableObjectAccess;
 
     protected Status $status;
+    protected Model $requestModel;
     protected Models $models;
 
-    /**
-     * @param TModel ...$models
-     */
-    public function __construct(Status $status, Model ...$models)
+    public function __construct(Status $status, Model $requestModel, Model ...$models)
     {
         $this->status = $status;
+        $this->requestModel = $requestModel;
         $this->models = new Models(...$models);
     }
 
     public function status(): Status
     {
         return $this->status;
+    }
+
+    public function requestModel(): Model
+    {
+        return $this->requestModel;
     }
 
     public function getIterator(): Traversable
