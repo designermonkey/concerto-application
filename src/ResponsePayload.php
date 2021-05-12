cc188f7397de9545d39f35cb732f2ad5e67a51ec
John Porter <john@designermonkey.co.uk>
2021-05-12 22:59:32 +0100

    reverting request model addition

--- src/ResponsePayload.php
+++ src/ResponsePayload.php
@@ -12,24 +12,17 @@ class ResponsePayload implements IteratorAggregate
     use ImmutableObjectAccess;
 
     protected Status $status;
-    protected Model $requestModel;
     protected Models $models;
 
-    public function __construct(Status $status, Model $requestModel, Model ...$models)
+    public function __construct(Status $status, Model ...$models)
     {
         $this->status = $status;
-        $this->requestModel = $requestModel;
         $this->models = new Models(...$models);
     }
 
     public function status(): Status
     {
         return $this->status;
-    }
-
-    public function requestModel(): Model
-    {
-        return $this->requestModel;
     }
 
     public function getIterator(): Traversable
